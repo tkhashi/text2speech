f7dee596253471ef8eb5009060216a36be86cf8e
Kazuhiro Takahashi <pupodenews@gmail.com>
2023-01-22 23:30:44 +0900

    Craete WPF v/vm/m

---
+++ csharp_wpf/TextToSpeechWPF/TextToSpeechWPF/MainWindowModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.TextToSpeech.V1;
+using TextToSpeech.AudioConfig;
+using TextToSpeech.SynthesisInputConfig;
+using TextToSpeech.VoiceSelection;
+
+namespace TextToSpeechWPF
+{
+    class MainWindowModel
+    {
+        public void Speech()
+        {
+            var gcEnv = "GOOGLE_APPLICATION_CREDENTIALS";
+            var credentias = @"./credentials.json";
+            Environment.SetEnvironmentVariable(gcEnv, credentias);
+
+            var client = TextToSpeechClient.Create();
+            var input = SynthesisInputFactory.Create();
+            var voiceSelection = VoiceSelectionFactory.Create();
+            var audioConfig = AudioConfigFactory.Create();
+
+            SynthesizeSpeechResponse response = client.SynthesizeSpeech(input, voiceSelection, audioConfig);
+
+            var outputDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            using var output = File.Create($"{outputDir}/sample.mp3");
+            response.AudioContent.WriteTo(output);
+        }
+    }
+}
---
+++ csharp_wpf/TextToSpeechWPF/TextToSpeechWPF/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Reactive.Bindings;
+
+namespace TextToSpeechWPF
+{
+    class MainWindowViewModel
+    {
+        private readonly MainWindowModel _model;
+
+        public ReactivePropertySlim<string> InputText { get; } = new();
+        public ReactivePropertySlim<double> SpeakingRate { get; } = new();
+        public ReactivePropertySlim<double> Pitch { get; } = new();
+        public ReactiveCommand SpeechCommand { get; } = new ();
+
+
+        public MainWindowViewModel()
+        {
+            _model = new MainWindowModel();
+            SpeechCommand.Subscribe(Speech);
+        }
+
+        private void Speech()
+        {
+            _model.Speech();
+        }
+    }
+}
