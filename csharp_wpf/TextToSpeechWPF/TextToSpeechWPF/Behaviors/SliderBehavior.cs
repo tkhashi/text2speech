f4bf97749fed877da5334b9e6693443c7e835a84
Kazuhiro Takahashi <pupodenews@gmail.com>
2023-02-25 22:53:20 +0900

    Sync slider by play audio without sync audio to drag slider

---
+++ csharp_wpf/TextToSpeechWPF/TextToSpeechWPF/Behaviors/SliderBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace TextToSpeechWPF.Behaviors
+{
+    class SliderBehavior : Behavior<Slider>
+    {
+        public double Value { get; set; }
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(double),
+            typeof(SliderBehavior),
+            new PropertyMetadata());
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.ValueChanged += ChangeValue;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.ValueChanged -= ChangeValue;
+
+        }
+
+        private void ChangeValue(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (e.OldValue == e.NewValue) return;
+            Value = e.NewValue;
+        }
+
+
+    }
+}
